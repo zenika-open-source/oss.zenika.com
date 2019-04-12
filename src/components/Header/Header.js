081eb0867c2dd25b17d76a6fd8dc700faddf1112
christopherkade <c.kade96@gmail.com>
2019-04-12 21:23:59 +0200

    🎨 Add index file to export our SocialLinks component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import SocialLinks from "../SocialLinks/SocialLinks"
+import { SocialLinks } from "../SocialLinks"
 import "./Header.css"
 
 const Header = ({ siteTitle }) => (
