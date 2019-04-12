54879da39a49617d34bcd485238397f64e968b7b
Thomas Betous <tbetous@users.noreply.github.com>
2019-04-12 09:33:51 +0200

    :sparkles: add elements to subscribe to netlify oss plan (#28)
    add elements to subscribe to netlify oss plan

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.css";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -31,23 +31,34 @@ const Layout = ({ children }) => (
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <div class="partners">
+              <div>
+                <a href="https://www.netlify.com">
+                  <img
+                    alt="Deploys by Netlify"
+                    src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"
+                  />
+                </a>
+              </div>
+            </div>
+            <div class="copyright">
+              © {new Date().getFullYear()}, Built with{" "}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </div>
           </footer>
         </div>
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
