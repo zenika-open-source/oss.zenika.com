45c2035e7ac5b37d24292f558e4b430babec1fb5
Benjamin Petetot <bpetetot@gmail.com>
2019-06-04 11:58:07 +0200

    :sparkles: hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,22 @@
-import React from "react"
+import React from "react";
 
-import logo from "./oss-logo.png"
-import "./Hero.css"
+import logo from "./logo.svg";
+import "./Hero.css";
 
-const Hero = () => {
-  return (
-    <article className="hero">
-      <img className="hero__logo" src={logo} alt="" />
-      <h1 className="hero__title">
-        Zenika + open-source = ♥
-      </h1>
-    </article>
-  )
-}
+const Hero = () => (
+  <article className="hero">
+    <img className="hero__logo" src={logo} alt="logo" />
+    <section className="hero__section">
+      <h1>&lt;Coding <strong>an open source</strong> World&gt;</h1>
+      <p>
+        Duis pulvinar tellus ullamcorper dui consequat ultricies. Duis
+        bibendum tincidunt ultricies.Aenean tincidunt accumsan aliquet. Nulla
+        sed eros vitae ligula suscipit consequat ornare vitae augue.Donec sed
+        sem a ligula finibus pellentesque. Vivamus nec sem faucibus, auctor
+        est non, sodales erat.
+      </p>
+    </section>
+  </article>
+);
 
-export default Hero
+export default Hero;
