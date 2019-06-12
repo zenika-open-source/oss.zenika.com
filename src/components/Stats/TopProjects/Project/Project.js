ff71057f66b401e1d0cbad42fe91d313c4da4126
Christopher Kade <c.kade96@gmail.com>
2019-06-12 13:02:30 +0200

    💄 Update top projects link placement (#52)

--- src/components/Stats/TopProjects/Project/Project.js
+++ src/components/Stats/TopProjects/Project/Project.js
@@ -4,15 +4,15 @@ import "./Project.css"
 
 const Project = ({ title, description, stars, url }) => {
   return (
-    <a href={url}>
-      <article className="project">
-        <div>
+    <article className="project">
+      <div>
+        <a href={url}>
           <div className="project__title">{title}</div>
-          <div className="project__description">{description}</div>
-        </div>
-        <div className="project__stars">{stars}</div>
-      </article>
-    </a>
+        </a>
+        <div className="project__description">{description}</div>
+      </div>
+      <div className="project__stars">{stars}</div>
+    </article>
   )
 }
 
