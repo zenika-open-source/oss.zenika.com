984bccc5df5809e7a4a883fc15bce39beaabad77
Christopher Kade <c.kade96@gmail.com>
2019-06-19 10:48:28 +0200

    ➖ Remove react-countup (#57)

--- src/components/Metrics/Metric/Metric.js
+++ src/components/Metrics/Metric/Metric.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import CountUp from 'react-countup'
 
 import "./Metric.css"
 
 const Metric = ({ value, text, image, subtitle }) => {
-  let displayedValue = value
-  if (value > 1000) {
-    displayedValue = new Intl.NumberFormat('en-US', { maximumSignificantDigits: 3 }).format(value / 1000)
-  }
+  let displayedValue = value > 1000 ? Math.trunc(value / 1000) : value
+
   return (
     <div className="metric">
       <img src={image} alt="text" />
       <div>
-        <span className="metric__value"><CountUp end={displayedValue} />{value > 1000 && 'K'}</span><span className="metric__title">{text}</span>
+        <span className="metric__value">
+          {displayedValue}{value > 1000 && 'K'}
+        </span>
+        <span className="metric__title">
+          {text}
+        </span>
       </div>
-      <div className="metric__subtitle">{subtitle}</div>
+      <p className="metric__subtitle">{subtitle}</p>
     </div>
   )
 }
