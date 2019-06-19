3eb6a0b4aae8b147684e62ba0c22bb8c137f717f
Benjamin Petetot <bpetetot@gmail.com>
2019-06-19 11:42:19 +0200

    :sparkles: set correct manifest (#67)
    Co-Authored-By: Yvonnick FRIN <frin.yvonnick@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Zenika Open Source`,
+        short_name: `OSS by Zenika`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
