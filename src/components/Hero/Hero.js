835d0b3e8a751473a166609cb9b99e3ca3974ae5
Benjamin Petetot <bpetetot@gmail.com>
2019-06-25 15:23:03 +0200

    :lipstick: add zenika and by zenika logos (#78)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 import { Logo } from "../Logo";
+import byZenika from "./byzenika.png";
+import zenika from "./zenika.png";
 import "./Hero.css";
 
 const Hero = () => (
   <article className="hero">
+    <img src={zenika} alt="zenika" width="60px" className="hero__zenika-logo" />
     <Logo className="hero__logo" />
     <section className="hero__section">
-      <h1>&lt;Coding <strong>an open source</strong> World&gt;</h1>
+      <div className="hero__title">
+        <h1>&lt;Coding <strong>an open source</strong> World&gt;</h1>
+        <img src={byZenika} alt="by Zenika" width="150px" />
+      </div>
       <p>
         Since the beginning, Zenika's DNA is based on open source philosophy. <br />
         Driven by this passion, we contribute and share more and more projects with communities. <br />
