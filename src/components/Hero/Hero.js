818310523995ff33154b132ab3f6591950e2902c
Benjamin Petetot <bpetetot@gmail.com>
2019-06-25 10:15:11 +0200

    :pencil2: fix homepage typos

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,8 +9,8 @@ const Hero = () => (
     <section className="hero__section">
       <h1>&lt;Coding <strong>an open source</strong> World&gt;</h1>
       <p>
-        Since the beginning, Zenika's DNA is based on open source philosophy. <br />
-        Driven by this passion, we contribute and share more and more projects with communities. <br />
+        Zenika's DNA is based on open source philosophy. <br />
+        Driven by this passion, we contribute to projects and share more and more with communities. <br />
       </p>
     </section>
   </article>
