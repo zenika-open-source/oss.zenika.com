91c22045c37e2e1f5b23440324a6424a2b0a5dd8
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-07-09 15:38:07 +0200

    :rotating_light: Fix indentation

--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -1,38 +1,41 @@
-import React from "react"
-import { Link } from "gatsby"
-import { Helmet } from "react-helmet"
-import Layout from '../components/layout'
-import { Hero } from '../components/Hero'
+import React from "react";
+import { Link } from "gatsby";
+import { Helmet } from "react-helmet";
+import Layout from "../components/layout";
+import { Hero } from "../components/Hero";
 
-import './BlogPost.css'; // make it pretty!
+import "./BlogPost.css";
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query we'll write in a bit
-}) {
-  const { markdownRemark: post } = data // data.markdownRemark holds our post data
-  const date = new Date(post.frontmatter.date)
+export default function Template({ data }) {
+  const { markdownRemark: post } = data;
+  const date = new Date(post.frontmatter.date);
   return (
-		<Layout noHero>
-			<Helmet title={`OSS by Zenika - ${post.frontmatter.title}`} />
+    <Layout noHero>
+      <Helmet title={`OSS by Zenika - ${post.frontmatter.title}`} />
       <Hero
-        title={(
+        title={
           <h1>
-            {post.frontmatter.title}<br />
-            <sub></sub>
+            {post.frontmatter.title}
+            <br />
+            <sub />
           </h1>
-        )}
+        }
         small
       />
-			<div className="blog-post">
-				<Link className="blog-post__back-home" to="/">Back to home</Link>
-				<div
-					className="blog-post-content"
-					dangerouslySetInnerHTML={{ __html: post.html }}
-				/>
-				<Link className="blog-post__back-home" to="/">Back to home</Link>
-			</div>
-		</Layout>
-  )
+      <div className="blog-post">
+        <Link className="blog-post__back-home" to="/">
+          Back to home
+        </Link>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+        <Link className="blog-post__back-home" to="/">
+          Back to home
+        </Link>
+      </div>
+    </Layout>
+  );
 }
 
 export const pageQuery = graphql`
@@ -43,8 +46,8 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         path
         title
-				author
+        author
       }
     }
   }
-`
+`;
