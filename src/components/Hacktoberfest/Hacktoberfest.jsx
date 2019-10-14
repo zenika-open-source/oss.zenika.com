4ea6fb517698ef228247c868e1622bd39f9fa8fc
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-10-14 17:02:52 +0200

    :ok_hand: Take care of review

--- src/components/Hacktoberfest/Hacktoberfest.jsx
+++ src/components/Hacktoberfest/Hacktoberfest.jsx
@@ -13,20 +13,22 @@ function Stat({ title, value }) {
 }
 
 export function Hacktoberfest() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState(null)
   useEffect(() => {
     fetch('https://europe-west1-github-insights-247314.cloudfunctions.net/hacktoberfest')
       .then(res => res.json())
       .then(setData)
+      .catch(() => setData(null))
   }, [])
 
-  console.log('data', data)
+  if (data === null) return null
+
   const numberOfParticipants = data.length
   const numberOfPullRequests = data.reduce((acc, { user }) => acc + user.contributionsCollection.pullRequestContributions.totalCount, 0)
-  const completionRate = data
+  const completionRate = Math.round(data
     .map(({ user }) => user.contributionsCollection.pullRequestContributions.totalCount)
-    .filter(pullRequestsTotal => pullRequestsTotal > 3)
-    .length / data.length * 100
+    .filter(pullRequestsTotal => pullRequestsTotal <= 4)
+    .length / data.length * 100)
 
   return (
     <div className="hacktoberfest">
