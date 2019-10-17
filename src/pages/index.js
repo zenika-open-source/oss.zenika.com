45af40e340f2dfe9bd18c5cb80b21468db49e465
Clément Fassot <kedketh@gmail.com>
2019-10-17 09:17:54 +0200

    :wrench: Add Prettier config and precommit hook (#98)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,27 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { Metrics } from "../components/Metrics"
-import { Stats } from "../components/Stats"
-import { NewProjects } from "../components/NewProjects"
-import { Resources } from "../components/Resources"
-import { Blog } from "../components/Blog"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { Metrics } from "../components/Metrics";
+import { Stats } from "../components/Stats";
+import { NewProjects } from "../components/NewProjects";
+import { Resources } from "../components/Resources";
+import { Blog } from "../components/Blog";
 
 const IndexPage = () => {
   return (
     <Layout>
-      <SEO title="OSS by Zenika" keywords={[`gatsby`, `application`, `react`]} />
+      <SEO
+        title="OSS by Zenika"
+        keywords={[`gatsby`, `application`, `react`]}
+      />
       <NewProjects />
       <Metrics />
       <Stats />
       <Blog />
       <Resources />
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
