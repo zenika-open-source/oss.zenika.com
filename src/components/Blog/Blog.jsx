057046957f25debb108b867aa233b6095df25c5a
Julien Landuré <julien.landure@gmail.com>
2019-11-25 04:43:50 -0800

    :pencil2: update blog text and links (#114)

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -9,14 +9,14 @@ export function Blog() {
       <h2>Open source white paper</h2>
       <div className="blog__content">
         <p className="blog__description">
-The world of open source is vast and have multiple facets. We are writing a white paper to help you on each aspects of open source projects. These articles are also open source feel free to give us feedback or participate on
+The world of open source is vast and have multiple facets. We are writing a white paper to help you on each aspects of open source projects. These articles are also open source feel free to give us feedback or participate on&nbsp;
           <a href="https://github.com/zenika-open-source/open-source-white-paper">our repository</a>
 .
         </p>
         <ul className="blog__chapters">
           <li className="blog__chapter"><Link to="/white-paper/getting-started">Chapter 1: Getting started</Link></li>
           <li className="blog__chapter"><Link to="/white-paper/preparing-your-project-being-open-sourced">Chapter 2: Preparing your project being open sourced</Link></li>
-          <li className="blog__chapter blog__chapter--disabled"><Link to="/white-paper/promotion">Chapter 3: Promotion</Link></li>
+          <li className="blog__chapter"><Link to="/white-paper/promotion">Chapter 3: Promotion</Link></li>
           <li className="blog__chapter blog__chapter--disabled">Chapter 4: Licenses</li>
           <li className="blog__chapter blog__chapter--disabled">Chapter 5: Maintaining</li>
           <li className="blog__chapter blog__chapter--disabled">Chapter 6: Recruitment</li>
